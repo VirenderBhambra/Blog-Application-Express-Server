19201b876973113a9f29a14e9bad34dcb08ee27c
Virender Bhambra <3940virenderbhambra@gmail.com>
2024-04-25 18:31:18 +0530

    src

---
+++ src/helper/crypto.js
@@ -0,0 +1,14 @@
+const CryptoJS = require("crypto-js");
+
+function decrypt(encryptedData) {
+  return JSON.parse(
+    CryptoJS.AES.decrypt(encryptedData, process.env.CRYPTO_SECRET).toString(
+      CryptoJS.enc.Utf8
+    )
+  );
+}
+
+function encrypt(data) {
+  return CryptoJS.AES.encrypt(JSON.stringify(data), process.env.CRYPTO_SECRET).toString();
+}
+module.exports = { encrypt, decrypt };
---
+++ src/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { Blog } = require("./schema");
+const { User } = require("./schema");
+const userRouter = require("./middleware/user");
+const blogRouter = require("./middleware/blog");
+const cookieParser = require("cookie-parser");
+dotenv.config();
+
+// const corsOptions = {
+//   exposedHeaders: 'Authorization',
+// };
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  methods: ['GET', 'POST','PUT','DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Authorization'],
+  credentials: true,
+}));
+// Use cookie-parser middleware
+app.use(cookieParser());
+
+// Set up middleware to parse JSON bodies
+app.use(express.json());
+
+
+require("./middleware/dbConnect");
+
+app.get('/',function (req, res) {
+  res.json('Hello World!');
+  })
+  
+app.use("/user", userRouter);
+app.use("/blog", blogRouter);
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
